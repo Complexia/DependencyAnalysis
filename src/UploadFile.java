68209acf83c82e6102bef471de4d5e45488fab1d
simon76 <s3353879@student.rmit.edu.au>
2016-04-18 16:54:07 +1000

    Merge conflicts on my local terminal fixed

--- src/UploadFile.java
+++ src/UploadFile.java
@@ -1,85 +1,14 @@
 
 import java.io.File;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-public class UploadFile
-{
-
-   public static void main(String argv[])
-   {
-
-      try
-      {
-
-         File fXmlFile = new File("/Users/SimonMiller/staff.xml");
-         DocumentBuilderFactory dbFactory =
-               DocumentBuilderFactory.newInstance();
-         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-         Document doc = dBuilder.parse(fXmlFile);
-
-         // optional, but recommended
-         // read this -
-         // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-         doc.getDocumentElement().normalize();
-
-         System.out.println("Root element :" +
-                            doc.getDocumentElement().getNodeName());
-
-         NodeList nList = doc.getElementsByTagName("staff");
-
-         System.out.println("----------------------------");
-
-         for (int temp = 0; temp < nList.getLength(); temp++)
-         {
-
-            Node nNode = nList.item(temp);
-
-            System.out.println("\nCurrent Element :" + nNode.getNodeName());
-
-            if (nNode.getNodeType() == Node.ELEMENT_NODE)
-            {
-
-               Element eElement = (Element) nNode;
-
-               System.out.println("Staff id : " + eElement.getAttribute("id"));
-               System.out.println("First Name : " +
-                                  eElement.getElementsByTagName("firstname")
-                                        .item(0).getTextContent());
-               System.out.println("Last Name : " +
-                                  eElement.getElementsByTagName("lastname")
-                                        .item(0).getTextContent());
-               System.out.println("Nick Name : " +
-                                  eElement.getElementsByTagName("nickname")
-                                        .item(0).getTextContent());
-               System.out.println("Salary : " + eElement
-                     .getElementsByTagName("salary").item(0).getTextContent());
-
-            }
-         }
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-      }
-   }
-
-}=======
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
-import java.io.File;
 import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class UploadFile
 {
@@ -103,7 +32,7 @@ public class UploadFile
       try
       {
 
-         File fXmlFile = new File("/Users/Roma/staff.xml");
+         File fXmlFile = new File("/Users/SimonMiller/test1.xml");
          DocumentBuilderFactory dbFactory =
                DocumentBuilderFactory.newInstance();
          DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -168,10 +97,10 @@ public class UploadFile
 
    }
 
-  public static void main(String argv[]) {
-	  
-	  doStuff();
+   public static void main(String argv[])
+   {
 
-   
+      doStuff();
+
+   }
 }
-}>>>>>>>76ef cd6858ae239406dac536cc5cb2340726b8ee
