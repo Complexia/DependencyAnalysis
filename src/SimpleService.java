132d7d240f4bad5b894fdc3daa2aab440e57eb53
simon76 <s3353879@student.rmit.edu.au>
2016-04-25 17:37:24 +1000

    Debugged xml reading funcionality

--- src/SimpleService.java
+++ src/SimpleService.java
@@ -1,45 +1,131 @@
 import java.util.ArrayList;
 
 public class SimpleService {
-	
+
 	private String name;
 	private String inputService;
 	private String outputService;
 	private String nameOfVariable;
-	
-	
-	
+	private String inputVariable;
+	private String outputVariable;
+
 	private ArrayList<String> elements;
-	
-	
-	public SimpleService(String name, String inputService, String outputService, String nameOfVariable){
-		
+
+	public SimpleService(String name, String inputService, String outputService, String nameOfVariable,
+			String outputVariable, String inputVariable) {
+
 		this.name = name;
 		this.inputService = inputService;
 		this.outputService = outputService;
 		this.nameOfVariable = nameOfVariable;
-		
+		this.inputVariable = inputVariable;
+		this.outputVariable = outputVariable;
+
 	}
 
-
+	/**
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	/**
+	 * @return the inputService
+	 */
 	public String getInputService() {
 		return inputService;
 	}
 
+	/**
+	 * @param inputService
+	 *            the inputService to set
+	 */
+	public void setInputService(String inputService) {
+		this.inputService = inputService;
+	}
 
+	/**
+	 * @return the outputService
+	 */
 	public String getOutputService() {
 		return outputService;
 	}
 
+	/**
+	 * @param outputService
+	 *            the outputService to set
+	 */
+	public void setOutputService(String outputService) {
+		this.outputService = outputService;
+	}
 
+	/**
+	 * @return the nameOfVariable
+	 */
 	public String getNameOfVariable() {
 		return nameOfVariable;
 	}
-	
+
+	/**
+	 * @param nameOfVariable
+	 *            the nameOfVariable to set
+	 */
+	public void setNameOfVariable(String nameOfVariable) {
+		this.nameOfVariable = nameOfVariable;
+	}
+
+	/**
+	 * @return the inputVariable
+	 */
+	public String getInputVariable() {
+		return inputVariable;
+	}
+
+	/**
+	 * @param inputVariable
+	 *            the inputVariable to set
+	 */
+	public void setInputVariable(String inputVariable) {
+		this.inputVariable = inputVariable;
+	}
+
+	/**
+	 * @return the outputVariable
+	 */
+	public String getOutputVariable() {
+		return outputVariable;
+	}
+
+	/**
+	 * @param outputVariable
+	 *            the outputVariable to set
+	 */
+	public void setOutputVariable(String outputVariable) {
+		this.outputVariable = outputVariable;
+	}
+
+	/**
+	 * @return the elements
+	 */
+	public ArrayList<String> getElements() {
+		return elements;
+	}
+
+	/**
+	 * @param elements
+	 *            the elements to set
+	 */
+	public void setElements(ArrayList<String> elements) {
+		this.elements = elements;
+	}
 
 }
--- src/UploadFile.java
+++ src/UploadFile.java
@@ -11,105 +11,91 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-public class UploadFile
-{
+public class UploadFile {
 
-   private static Document doc1;
-   private static ArrayList<Service> elements = new ArrayList();
-   private static ArrayList<String> variables = new ArrayList();
-   private static HashMap<String,String> variablesMap = new HashMap<>();
+	private static Document doc1;
+	private static ArrayList<Service> elements = new ArrayList();
+	private static ArrayList<String> variables = new ArrayList();
+	private static HashMap<String, String> variablesMap = new HashMap<>();
 
-   public static Document getDoc()
-   {
-      return doc1;
-   }
+	public static Document getDoc() {
+		return doc1;
+	}
 
-   public static ArrayList<Service> getElements()
-   {
-      return elements;
-   }
-   
-   public  static HashMap getVariablesMap(){
-	   return variablesMap;
-   }
+	public static ArrayList<Service> getElements() {
+		return elements;
+	}
 
-   public static void uploadL0(File file)
-   {
-      try
-      {
+	public static HashMap getVariablesMap() {
+		return variablesMap;
+	}
 
-         File fXmlFile = file;
-         DocumentBuilderFactory dbFactory =
-               DocumentBuilderFactory.newInstance();
-         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-         Document doc = dBuilder.parse(fXmlFile);
+	public static void uploadL0(File file) {
+		try {
 
-         // optional, but recommended
-         // read this -
-         // http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
-         doc.getDocumentElement().normalize();
-         System.out.println("Root element :" 
-                 + doc.getDocumentElement().getNodeName());
-         NodeList nList = doc.getElementsByTagName("service");
-         for (int temp = 0; temp < nList.getLength(); temp++) {
-             Node nNode = nList.item(temp);
-             System.out.println("\nCurrent Element :" 
-                + nNode.getNodeName());
-             
-             
-             if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                 Element eElement = (Element) nNode;
-                 
-                 System.out.println("Name of Service : "
-                   + eElement.getAttribute("serviceName"));
-                 
-                 variablesMap.put("nameOfService", eElement.getAttribute("serviceName"));
-                
-                 
-                 System.out.println("Input Service : " 
-                         + eElement
-                         .getElementsByTagName("input_service")
-                         .item(0)
-                         .getTextContent());
-                 
-                 variablesMap.put("inputService", eElement
-                         .getElementsByTagName("input_service")
-                         .item(0)
-                         .getTextContent());
-                 
-                 System.out.println("Output Service : " 
-                         + eElement
-                         .getElementsByTagName("output_service")
-                         .item(0)
-                         .getTextContent());
-                 
-                 variablesMap.put("outputService", eElement
-                         .getElementsByTagName("output_service")
-                         .item(0)
-                         .getTextContent());
-                 
-                 System.out.println("Name of Variable : " 
-                         + eElement
-                         .getElementsByTagName("nameofvariable")
-                         .item(0)
-                         .getTextContent());
-                 
-                 variablesMap.put("nameOfVariable", eElement
-                         .getElementsByTagName("nameofvariable")
-                         .item(0)
-                         .getTextContent());
-             }
-             
-         }
+			File fXmlFile = file;
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+			Document doc = dBuilder.parse(fXmlFile);
 
-         doc1 = doc;
-      }
-      catch (Exception e)
-      {
-         e.printStackTrace();
-      }
+			// optional, but recommended
+			// read this -
+			// http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+			doc.getDocumentElement().normalize();
+			System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+			NodeList nList = doc.getElementsByTagName("service");
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				System.out.println("\nCurrent Element :" + nNode.getNodeName());
 
-   }
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
 
-   
+					System.out.println("Name of Service : "
+							+ eElement.getElementsByTagName("service_name").item(0).getTextContent());
+
+					variablesMap.put("nameOfService",
+							eElement.getElementsByTagName("service_name").item(0).getTextContent());
+
+					System.out.println("Input Service : "
+							+ eElement.getElementsByTagName("input_service").item(0).getTextContent());
+
+					variablesMap.put("inputService",
+							eElement.getElementsByTagName("input_service").item(0).getTextContent());
+
+					System.out.println("Output Service : "
+							+ eElement.getElementsByTagName("output_service").item(0).getTextContent());
+
+					variablesMap.put("outputService",
+							eElement.getElementsByTagName("output_service").item(0).getTextContent());
+
+					System.out.println("Name of Variable : "
+							+ eElement.getElementsByTagName("nameofvariable").item(0).getTextContent());
+
+					variablesMap.put("nameOfVariable",
+							eElement.getElementsByTagName("nameofvariable").item(0).getTextContent());
+
+					System.out.println("Input Variable : "
+							+ eElement.getElementsByTagName("input_variable").item(0).getTextContent());
+
+					variablesMap.put("inputVariable",
+							eElement.getElementsByTagName("input_variable").item(0).getTextContent());
+
+					System.out.println("Output Variable : "
+							+ eElement.getElementsByTagName("output_variable").item(0).getTextContent());
+
+					variablesMap.put("outputVariable",
+							eElement.getElementsByTagName("output_variable").item(0).getTextContent());
+
+				}
+
+			}
+
+			doc1 = doc;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
