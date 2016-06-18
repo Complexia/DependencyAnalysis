edd11fd9240be11309578df4a33db0cb6c2bfa79
Jordan <s3423620@student.rmit.edu.au>
2016-06-18 14:15:45 +1000

    commented out unused code

--- src/controller/RemoteComHighLoadListener.java
+++ src/controller/RemoteComHighLoadListener.java
@@ -190,6 +190,7 @@ public class RemoteComHighLoadListener implements ActionListener{
 		}
 	}
 	/**
+	 * @deprecated
 	 * @param SCS Strongly connected service index
 	 * @return the Remote service
 	 */
@@ -205,202 +206,209 @@ public class RemoteComHighLoadListener implements ActionListener{
 		}
 		return j;
 	}
-	public int FindSCSbyString(String AL1) {
-		int k = 0;
-		for (ArrayList<StronglyConnectedService> SCSList : RSList2) {
-			for (StronglyConnectedService SCS2 : SCSList) {
-				for (ServiceNode SN : SCS2.getServiceNodes()) {
-					if (SN.getService().getName().equals(AL1)) {
-						k = SCS2.index;
-					}
-				}
-			}
-		}
-		return k;
-	}
-	/**
-	 * looks for a link between two services in the Strongly Connected Service by the link Data
-	 * @param SCS the Strongly Connected Service to be checked
-	 * @param Data the service we are checking for
-	 * @return whether the search found a link
-	 */
-	public boolean CheckInternal(StronglyConnectedService SCS, String Data) {
-		//the way this is functioning i think we need to make it so it looks at the services themselves not the IOs connecting them
-		ArrayList<ServiceNode> SCSN = new ArrayList<ServiceNode>();
-		SCSN = SCS.getServiceNodes();
-		for (int i = 0; i < SCSN.size(); i++) {
-			for (int j = SCSN.size() - 1; j > i; j--) {
-				if ((SCSN.get(i).service.getOutputService().contains(Data) && (SCSN.get(j).service.getInputService().contains(Data)))) {
-					return true;
-				} else if ((SCSN.get(i).service.getInputService().contains(Data)
-						&& (SCSN.get(j).service.getOutputService().contains(Data)))) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	/**
-	 * @param ALElementary the service used to find the appropriate SCSs
-	 * @return a list of high load output SCSs
-	 */
-	public ArrayList<StronglyConnectedService> getL2HLO(String ALElementary) {
-		ArrayList<StronglyConnectedService> ALOut = new ArrayList<StronglyConnectedService>();
-		for (StronglyConnectedService scs : scsList) {
-			if ((scs.getAllOutputs().contains(ALElementary))) {
-				if (!CheckInternal(scs, ALElementary)) {
-					ALOut.add(scs);
-				}
-			}
-		}
-		return ALOut;
-	}
-	public ArrayList<StronglyConnectedService> getL2HLI(String ALElementary) {
-		ArrayList<StronglyConnectedService> ALOut = new ArrayList<StronglyConnectedService>();
-		for (StronglyConnectedService scs : scsList) {
-			if ((scs.getAllInputs().contains(ALElementary))) {
-				if (!CheckInternal(scs, ALElementary)) {
-					ALOut.add(scs);
-				}
-			}
-		}
-		return ALOut;
-	}
-	private boolean CheckContain2(ArrayList<StronglyConnectedService> A1, ArrayList<StronglyConnectedService> A2) {
-		for (StronglyConnectedService E : A2) {
-			if (A1.contains(E)) {
-				return true;
-			}
-		}
-		return false;
-	}
-	private ArrayList<ArrayList<StronglyConnectedService>> RefineData(
-			ArrayList<ArrayList<StronglyConnectedService>> DataList) {
-		ArrayList<ArrayList<StronglyConnectedService>> DataMerge = new ArrayList<ArrayList<StronglyConnectedService>>();
-		for (int i = 0; i < DataList.size(); i++) {
-			for (int j = DataList.size() - 1; j > i; j--) {
-				if ((DataList.get(i) == DataList.get(j))) {
-					DataList.remove(j);
-				} else if (DataList.get(i).containsAll(DataList.get(j))) {
-					DataList.remove(j);
-				} else if (DataList.get(i).size() == 0) {
-					DataList.remove(i);
-				}
-			}
-		}
-		DataMerge = DataList;
-		return DataMerge;
-	}
-	
-	private ArrayList<StronglyConnectedService> Combine(ArrayList<StronglyConnectedService> A1,
-			ArrayList<StronglyConnectedService> A2) {
-		ArrayList<StronglyConnectedService> combine = new ArrayList<StronglyConnectedService>();
-		combine.addAll(A1);
-		combine.addAll(A2);
-		removeDuplicateSCS(combine);
-		return combine;
-	}
-	public void removeDuplicateSCS(ArrayList<StronglyConnectedService> Combine) {
-		// Combine.sort(null);
-		for (int i = 0; i < Combine.size(); i++) {
-			for (int j = Combine.size() - 1; j > i; j--) {
-				if (Combine.get(i) == Combine.get(j)) {
-					Combine.remove(j);
-				}
-			}
-		}
-		// return OutputMerge2;
-	}
-	
-	
-	private void RemoteComputation(ArrayList<String> AL) {
-		ArrayList<StronglyConnectedService> TotalOringinal = new ArrayList<StronglyConnectedService>();
-		
-		scsList = new ArrayList<StronglyConnectedService>();
-		for (StronglyConnectedService scs : scsList) {
-			TotalOringinal.add(scs);
-		}
-		// Create a ArrayList to Store Service List for High Load Input
-		ArrayList<StronglyConnectedService>[] L2HLI = new ArrayList[AL.size()];
-		// Create a ArrayList to Store Service List for High Load Output
-		ArrayList<StronglyConnectedService>[] L2HLO = new ArrayList[AL.size()];
-		ArrayList<ArrayList<StronglyConnectedService>> L2HE = new ArrayList<ArrayList<StronglyConnectedService>>();
-		ArrayList<StronglyConnectedService> Total = new ArrayList<StronglyConnectedService>();
-		ArrayList<StronglyConnectedService> Combine = new ArrayList<StronglyConnectedService>();
-		for (int i = 0; i < AL.size(); i++) {
-			L2HLI[i] = getL2HLI(AL.get(i));
-			L2HLO[i] = getL2HLO(AL.get(i));
-			ArrayList<StronglyConnectedService> DataMerge = new ArrayList<StronglyConnectedService>();
-			DataMerge.addAll(L2HLI[i]);
-			DataMerge.addAll(L2HLO[i]);
-			L2HE.add(DataMerge);
-		}
-
-		ArrayList<StronglyConnectedService> Mark1 = null;
-		ArrayList<StronglyConnectedService> Mark2 = null;
-		// Remove the empty element of the Array
-		for (int i = 0; i < L2HE.size(); i++) {
-			for (int j = L2HE.size() - 1; j > i; j--) {
-				if ((L2HE.get(i).isEmpty())) {
-					L2HE.remove(i);
-				}
-				if ((L2HE.get(j).isEmpty())) {
-					L2HE.remove(j);
-				}
-			}
-		}
-
-		// Combine the two elements and mark the Two should be deleted
-		for (int i = 0; i < L2HE.size(); i++) {
-			for (int j = L2HE.size() - 1; j > i; j--) {
-				if (CheckContain2(L2HE.get(i), L2HE.get(j))) {
-					Combine = Combine(L2HE.get(i), L2HE.get(j));
-					RSList.add(Combine);
-					Mark1 = L2HE.get(i);
-					Mark2 = L2HE.get(j);
-				}
-			}
-		}
-
-		L2HE.remove(Mark1);
-		L2HE.remove(Mark2);
-		RSList.addAll(L2HE);
-
-		for (ArrayList<StronglyConnectedService> RSE : RefineData(RSList)) {
-			Total.addAll(RSE);
-		}
-
-		ArrayList<StronglyConnectedService> Diff = new ArrayList<StronglyConnectedService>();
-
-		TotalOringinal.removeAll(Total);
-		for (StronglyConnectedService SCS1 : Total) {
-			if (TotalOringinal.contains(SCS1)) {
-				TotalOringinal.remove(SCS1);
-			}
-		}
-		Diff = TotalOringinal;
-
-		RSList.add(Diff);
-
-		columnNames = new Object[] { "Service Name", "High Load Component", "Group name", "InputData", "OutputData" };
-		tableData = new Object[20][columnNames.length];
-		int index, rowIndex = 0;
-		int index2 = 1;
-
-		for (ArrayList<StronglyConnectedService> SCSList : RSList) {
-			for (StronglyConnectedService SCS : SCSList) {
-				index = 0;
-				tableData[rowIndex][index++] = "S" + SCS.getIndex();
-				tableData[rowIndex][index++] = "YES";
-				tableData[rowIndex][index++] = "RS" + index2;
-				tableData[rowIndex][index++] = SCS.getAllInputs();
-				tableData[rowIndex][index++] = SCS.getAllOutputs();
-				rowIndex++;
-			}
-			index2++;
-		}
-		Factory.displayResult(tableData, columnNames);
-	}
+//	/**
+//	 * @deprecated
+//	 * @param AL1
+//	 * @return the strongly connected service
+//	 */
+//	public int FindSCSbyString(String AL1) {
+//		int k = 0;
+//		for (ArrayList<StronglyConnectedService> SCSList : RSList2) {
+//			for (StronglyConnectedService SCS2 : SCSList) {
+//				for (ServiceNode SN : SCS2.getServiceNodes()) {
+//					if (SN.getService().getName().equals(AL1)) {
+//						k = SCS2.index;
+//					}
+//				}
+//			}
+//		}
+//		return k;
+//	}
+//	/**
+//	 * looks for a link between two services in the Strongly Connected Service by the link Data
+//	 * @deprecated
+//	 * @param SCS the Strongly Connected Service to be checked
+//	 * @param Data the service we are checking for
+//	 * @return whether the search found a link
+//	 */
+//	public boolean CheckInternal(StronglyConnectedService SCS, String Data) {
+//		//the way this is functioning i think we need to make it so it looks at the services themselves not the IOs connecting them
+//		ArrayList<ServiceNode> SCSN = new ArrayList<ServiceNode>();
+//		SCSN = SCS.getServiceNodes();
+//		for (int i = 0; i < SCSN.size(); i++) {
+//			for (int j = SCSN.size() - 1; j > i; j--) {
+//				if ((SCSN.get(i).service.getOutputService().contains(Data) && (SCSN.get(j).service.getInputService().contains(Data)))) {
+//					return true;
+//				} else if ((SCSN.get(i).service.getInputService().contains(Data)
+//						&& (SCSN.get(j).service.getOutputService().contains(Data)))) {
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
+//	}
+//	
+//	/**
+//	 * @deprecated
+//	 * @param ALElementary the service used to find the appropriate SCSs
+//	 * @return a list of high load output SCSs
+//	 */
+//	public ArrayList<StronglyConnectedService> getL2HLO(String ALElementary) {
+//		ArrayList<StronglyConnectedService> ALOut = new ArrayList<StronglyConnectedService>();
+//		for (StronglyConnectedService scs : scsList) {
+//			if ((scs.getAllOutputs().contains(ALElementary))) {
+//				if (!CheckInternal(scs, ALElementary)) {
+//					ALOut.add(scs);
+//				}
+//			}
+//		}
+//		return ALOut;
+//	}
+//	public ArrayList<StronglyConnectedService> getL2HLI(String ALElementary) {
+//		ArrayList<StronglyConnectedService> ALOut = new ArrayList<StronglyConnectedService>();
+//		for (StronglyConnectedService scs : scsList) {
+//			if ((scs.getAllInputs().contains(ALElementary))) {
+//				if (!CheckInternal(scs, ALElementary)) {
+//					ALOut.add(scs);
+//				}
+//			}
+//		}
+//		return ALOut;
+//	}
+//	private boolean CheckContain2(ArrayList<StronglyConnectedService> A1, ArrayList<StronglyConnectedService> A2) {
+//		for (StronglyConnectedService E : A2) {
+//			if (A1.contains(E)) {
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+//	private ArrayList<ArrayList<StronglyConnectedService>> RefineData(
+//			ArrayList<ArrayList<StronglyConnectedService>> DataList) {
+//		ArrayList<ArrayList<StronglyConnectedService>> DataMerge = new ArrayList<ArrayList<StronglyConnectedService>>();
+//		for (int i = 0; i < DataList.size(); i++) {
+//			for (int j = DataList.size() - 1; j > i; j--) {
+//				if ((DataList.get(i) == DataList.get(j))) {
+//					DataList.remove(j);
+//				} else if (DataList.get(i).containsAll(DataList.get(j))) {
+//					DataList.remove(j);
+//				} else if (DataList.get(i).size() == 0) {
+//					DataList.remove(i);
+//				}
+//			}
+//		}
+//		DataMerge = DataList;
+//		return DataMerge;
+//	}
+//	
+//	private ArrayList<StronglyConnectedService> Combine(ArrayList<StronglyConnectedService> A1,
+//			ArrayList<StronglyConnectedService> A2) {
+//		ArrayList<StronglyConnectedService> combine = new ArrayList<StronglyConnectedService>();
+//		combine.addAll(A1);
+//		combine.addAll(A2);
+//		removeDuplicateSCS(combine);
+//		return combine;
+//	}
+//	public void removeDuplicateSCS(ArrayList<StronglyConnectedService> Combine) {
+//		// Combine.sort(null);
+//		for (int i = 0; i < Combine.size(); i++) {
+//			for (int j = Combine.size() - 1; j > i; j--) {
+//				if (Combine.get(i) == Combine.get(j)) {
+//					Combine.remove(j);
+//				}
+//			}
+//		}
+//		// return OutputMerge2;
+//	}
+//	
+//	
+//	private void RemoteComputation(ArrayList<String> AL) {
+//		ArrayList<StronglyConnectedService> TotalOringinal = new ArrayList<StronglyConnectedService>();
+//		
+//		scsList = new ArrayList<StronglyConnectedService>();
+//		for (StronglyConnectedService scs : scsList) {
+//			TotalOringinal.add(scs);
+//		}
+//		// Create a ArrayList to Store Service List for High Load Input
+//		ArrayList<StronglyConnectedService>[] L2HLI = new ArrayList[AL.size()];
+//		// Create a ArrayList to Store Service List for High Load Output
+//		ArrayList<StronglyConnectedService>[] L2HLO = new ArrayList[AL.size()];
+//		ArrayList<ArrayList<StronglyConnectedService>> L2HE = new ArrayList<ArrayList<StronglyConnectedService>>();
+//		ArrayList<StronglyConnectedService> Total = new ArrayList<StronglyConnectedService>();
+//		ArrayList<StronglyConnectedService> Combine = new ArrayList<StronglyConnectedService>();
+//		for (int i = 0; i < AL.size(); i++) {
+//			L2HLI[i] = getL2HLI(AL.get(i));
+//			L2HLO[i] = getL2HLO(AL.get(i));
+//			ArrayList<StronglyConnectedService> DataMerge = new ArrayList<StronglyConnectedService>();
+//			DataMerge.addAll(L2HLI[i]);
+//			DataMerge.addAll(L2HLO[i]);
+//			L2HE.add(DataMerge);
+//		}
+//
+//		ArrayList<StronglyConnectedService> Mark1 = null;
+//		ArrayList<StronglyConnectedService> Mark2 = null;
+//		// Remove the empty element of the Array
+//		for (int i = 0; i < L2HE.size(); i++) {
+//			for (int j = L2HE.size() - 1; j > i; j--) {
+//				if ((L2HE.get(i).isEmpty())) {
+//					L2HE.remove(i);
+//				}
+//				if ((L2HE.get(j).isEmpty())) {
+//					L2HE.remove(j);
+//				}
+//			}
+//		}
+//
+//		// Combine the two elements and mark the Two should be deleted
+//		for (int i = 0; i < L2HE.size(); i++) {
+//			for (int j = L2HE.size() - 1; j > i; j--) {
+//				if (CheckContain2(L2HE.get(i), L2HE.get(j))) {
+//					Combine = Combine(L2HE.get(i), L2HE.get(j));
+//					RSList.add(Combine);
+//					Mark1 = L2HE.get(i);
+//					Mark2 = L2HE.get(j);
+//				}
+//			}
+//		}
+//
+//		L2HE.remove(Mark1);
+//		L2HE.remove(Mark2);
+//		RSList.addAll(L2HE);
+//
+//		for (ArrayList<StronglyConnectedService> RSE : RefineData(RSList)) {
+//			Total.addAll(RSE);
+//		}
+//
+//		ArrayList<StronglyConnectedService> Diff = new ArrayList<StronglyConnectedService>();
+//
+//		TotalOringinal.removeAll(Total);
+//		for (StronglyConnectedService SCS1 : Total) {
+//			if (TotalOringinal.contains(SCS1)) {
+//				TotalOringinal.remove(SCS1);
+//			}
+//		}
+//		Diff = TotalOringinal;
+//
+//		RSList.add(Diff);
+//
+//		columnNames = new Object[] { "Service Name", "High Load Component", "Group name", "InputData", "OutputData" };
+//		tableData = new Object[20][columnNames.length];
+//		int index, rowIndex = 0;
+//		int index2 = 1;
+//
+//		for (ArrayList<StronglyConnectedService> SCSList : RSList) {
+//			for (StronglyConnectedService SCS : SCSList) {
+//				index = 0;
+//				tableData[rowIndex][index++] = "S" + SCS.getIndex();
+//				tableData[rowIndex][index++] = "YES";
+//				tableData[rowIndex][index++] = "RS" + index2;
+//				tableData[rowIndex][index++] = SCS.getAllInputs();
+//				tableData[rowIndex][index++] = SCS.getAllOutputs();
+//				rowIndex++;
+//			}
+//			index2++;
+//		}
+//		Factory.displayResult(tableData, columnNames);
+//	}
 
 }
